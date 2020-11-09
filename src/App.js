0649dd8a37d67cf5b681d940ee07ff840f1065d1
Timothy Boudreau <timothy.boudreau3@gmail.com>
2020-11-09 17:02:14 -0600

    Changed "COVID-19" Banner Image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 
-import coronaImage from './images/covid-19.png'
+import coronaImage from './images/COVID-19-alternative.png'
 class App extends React.Component {
     state = {
         data: {},
